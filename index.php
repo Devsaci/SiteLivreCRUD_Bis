f64a7d5dea8ed06ec986cac5ae9054e913cc4148
saci zakaria <sacideveloppeur@gmail.com>
2020-11-03 15:57:00 +0100

    Page Listant livres

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
 
-    require "views/accueil.views.php";
-
+if(empty($_GET['page'])){
+    require "views/accueil.view.php";
+} else {
+    switch($_GET['page']){
+        case "accueil" : require "views/accueil.view.php";
+        break;
+        case "livres" : require "views/livres.view.php";
+        break;
+    }
+}
